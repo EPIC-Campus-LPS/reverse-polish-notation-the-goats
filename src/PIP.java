482e8bfabe5d66bcae0e878cd9f7528d4b1873eb
nicholas-shortlastname <nicholasu750@lpsk12.org>
2023-09-26 10:01:53 -0600

    checkPrescedence worked on

--- src/PIP.java
+++ src/PIP.java
@@ -1,11 +1,13 @@
 import java.util.Stack;
 import java.util.Scanner;
+import java.util.HashMap;
 
 public class PIP {
 
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
 
     }
 
@@ -22,61 +24,31 @@ public class PIP {
 
         for (int i = 0; i < input.length(); i++) {
 
-            if (s.empty()) {
+            if (s.empty() && input.charAt(i) == '+'
+                    || input.charAt(i) == '-'
+                    || input.charAt(i) == '*'
+                    || input.charAt(i) == '/'
+                    || input.charAt(i) == '^') {
 
-                if (input.charAt(i) == '+') {
-
-                    s.push(input.charAt(i));
-
-                } else if (input.charAt(i) == '-') {
-
-                    s.push(input.charAt(i));
-
-                } else if (input.charAt(i) == '*') {
-
-                    s.push(input.charAt(i));
-
-                } else if (input.charAt(i) == '/') {
-
-                    s.push(input.charAt(i));
-
-                } else if (input.charAt(i) == '^') {
-
-                    s.push(input.charAt(i));
-
-                } else {
-
-                    result += input.charAt(i);
-
-                }
+                s.push(input.charAt(i));
 
             } else {
 
-                if (input.charAt(i) == '+') {
+                result += input.charAt(i);
 
+            }
 
+            if (input.charAt(i) == '+'
+                    || input.charAt(i) == '-'
+                    || input.charAt(i) == '*'
+                    || input.charAt(i) == '/'
+                    || input.charAt(i) == '^') {
 
-                } else if (input.charAt(i) == '-') {
+                checkPrecedence(input.charAt(i),s,result);
 
+            } else {
 
-
-                } else if (input.charAt(i) == '*') {
-
-
-
-                } else if (input.charAt(i) == '/') {
-
-
-
-                } else if (input.charAt(i) == '^') {
-
-
-
-                } else {
-
-                    result += input.charAt(i);
-
-                }
+                result += input.charAt(i);
 
             }
 
@@ -86,10 +58,25 @@ public class PIP {
 
     }
 
-    static void checkPrecedence(char oper) {
 
+    static void checkPrecedence(char oper, Stack stack, String output) {
 
+        HashMap<Character, Integer> precedence = new HashMap<Character, Integer>();
 
+        precedence.put('^',1);
+        precedence.put('*',2);
+        precedence.put('/',2);
+        precedence.put('-',3);
+        precedence.put('+',3);
+
+        while(!stack.empty()) {
+            if (precedence.get(oper) > precedence.get(stack.peek())) {
+                stack.push(oper);
+            } else {
+                output += stack.pop();
+                break;
+            }
+        }
     }
 
 }
